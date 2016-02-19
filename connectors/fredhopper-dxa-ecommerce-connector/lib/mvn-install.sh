153132064e97bffb40180f7b9fac3fe4919ceece
Niclas Cedermalm <niclas@cedermalm.se>
2016-02-19 11:44:07 +0100

    Added Fredhopper client JARs.

---
+++ connectors/fredhopper-dxa-ecommerce-connector/lib/mvn-install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Install Fredhopper libraries in the local Maven repository
+
+echo "Installing Fredhopper libraries into the local Maven repository..."
+
+mvn -q install:install-file -DgroupId=com.fredhopper -DartifactId=ws-client -Dversion=7.5.2.7 -Dpackaging=jar -Dfile=ws-client-7.5.2.7.jar
+mvn -q install:install-file -DgroupId=com.fredhopper -DartifactId=query-lang -Dversion=7.5.2.7 -Dpackaging=jar -Dfile=query-lang-7.5.2.7.jar
+
+echo "Finished"
