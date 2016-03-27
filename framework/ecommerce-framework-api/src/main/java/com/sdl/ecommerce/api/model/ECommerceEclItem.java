141257d1d597767aff0a9badd9b9febd29f16045
Niclas Cedermalm <niclas@cedermalm.se>
2016-03-27 09:59:00 +0200

    Minor update.

--- framework/ecommerce-framework-api/src/main/java/com/sdl/ecommerce/api/model/ECommerceEclItem.java
+++ framework/ecommerce-framework-api/src/main/java/com/sdl/ecommerce/api/model/ECommerceEclItem.java
@@ -10,14 +10,14 @@ import com.sdl.webapp.common.api.model.entity.EclItem;
 public class ECommerceEclItem extends EclItem {
 
     /**
-     * @return get external ID of the ECL item (category or product ID)
+     * @return external ID of the ECL item (category or product ID)
      */
     public String getExternalId() {
         return (String) this.getExternalMetadata().get("Id");
     }
 
     /**
-     * @return get name used by the external system, such as category title, product name etc
+     * @return name used by the external system, such as category title, product name etc
      */
     public String getExternalName() {
         return (String) this.getExternalMetadata().get("Name");
