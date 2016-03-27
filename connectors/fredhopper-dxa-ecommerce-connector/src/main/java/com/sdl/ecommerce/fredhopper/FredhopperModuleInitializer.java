808d2239a0e436048f0ac1dcdd92680c6f7ebac4
Niclas Cedermalm <niclas@cedermalm.se>
2016-03-27 09:55:13 +0200

    Prepared for Fredhopper ECL.

--- connectors/fredhopper-dxa-ecommerce-connector/src/main/java/com/sdl/ecommerce/fredhopper/FredhopperModuleInitializer.java
+++ connectors/fredhopper-dxa-ecommerce-connector/src/main/java/com/sdl/ecommerce/fredhopper/FredhopperModuleInitializer.java
@@ -1,9 +1,13 @@
 package com.sdl.ecommerce.fredhopper;
 
+import com.sdl.ecommerce.api.model.ECommerceEclItem;
 import com.sdl.webapp.common.api.mapping.views.AbstractInitializer;
+import com.sdl.webapp.common.api.mapping.views.RegisteredView;
+import com.sdl.webapp.common.api.mapping.views.RegisteredViews;
 import com.sdl.webapp.common.markup.PluggableMarkupRegistry;
 import com.sdl.webapp.common.markup.html.builders.HtmlBuilders;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 
@@ -12,6 +16,10 @@ import javax.annotation.PostConstruct;
  *
  * @author nic
  */
+@Component
+@RegisteredViews({
+        @RegisteredView(viewName = "ExternalContentLibraryStubSchemafredhopper", clazz = ECommerceEclItem.class)
+})
 public class FredhopperModuleInitializer extends AbstractInitializer {
 
     private static final String AREA_NAME = "Fredhopper";
