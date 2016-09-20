b23a9e70a88a0f7e3caecd53e767f85083a5c2fd
Niclas Cedermalm <niclas@cedermalm.se>
2016-09-20 22:45:31 +0200

    Corrected assembly name on ECL connector.

--- ecl/fredhopper-ecl-provider/fredhopper-ecl-provider/Properties/AssemblyInfo.cs
+++ ecl/fredhopper-ecl-provider/fredhopper-ecl-provider/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("hybris-ecl-provider")]
+[assembly: AssemblyTitle("fredhopper-ecl-provider")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("hybris-ecl-provider")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyProduct("fredhopper-ecl-provider")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
