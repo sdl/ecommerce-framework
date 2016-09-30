2bd4a10fa3f1b4755bfb898077205c76e39055a2
Niclas Cedermalm <niclas@cedermalm.se>
2016-10-01 00:24:29 +0200

    Added interface to ECommerceClient.

---
+++ dxa.net/ecommerce-framework-dotnet/SDL.ECommerce.Api/IECommerceClient.cs
@@ -0,0 +1,14 @@
+﻿using SDL.ECommerce.Api.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDL.ECommerce.Api
+{
+    public interface IECommerceClient
+    {
+        IProductDetailService DetailService { get; }
+    }
+}
