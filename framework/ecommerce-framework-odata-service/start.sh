ae75d0422468cddfdea4e66a8f54536eddb28a55
Niclas Cedermalm <niclas@cedermalm.se>
2016-10-19 01:04:52 +0200

    Added some simple start scripts for the micro service.

---
+++ framework/ecommerce-framework-odata-service/start.sh
@@ -0,0 +1,1 @@
+java -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true -Dorg.apache.catalina.connector.CoyoteAdapter.ALLOW_BACKSLASH=true -Dfile.encoding=UTF-8 -jar target/ecommerce-framework-odata-service-1.1.0.jar
