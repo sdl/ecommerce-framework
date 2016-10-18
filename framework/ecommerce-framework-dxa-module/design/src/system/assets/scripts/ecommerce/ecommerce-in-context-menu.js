3e8f9b3d45c1956b78067df378744e0460db780b
Niclas Cedermalm <niclas@cedermalm.se>
2016-10-18 18:40:15 +0200

    Corrected the E-Commerce javascripts in the HTML design package

--- framework/ecommerce-framework-dxa-module/design/src/system/assets/scripts/ecommerce/ecommerce-in-context-menu.js
+++ framework/ecommerce-framework-dxa-module/design/src/system/assets/scripts/ecommerce/ecommerce-in-context-menu.js
@@ -1,7 +1,9 @@
-//
-// Move XPM create new menu to the header (temporary workaround)
-//
-if ($(".xpm-create-new-menu").length > 0) {
-    $("#page-header .page-border").css("position", "relative");
-    $(".xpm-create-new-menu").detach().appendTo("#page-header .page-border");
-}
+$(document).ready(function() {
+    //
+    // Move XPM create new menu to the header (temporary workaround)
+    //
+    if ($(".xpm-create-new-menu").length > 0) {
+        $("#page-header .page-border").css("position", "relative");
+        $(".xpm-create-new-menu").detach().appendTo("#page-header .page-border");
+    }
+});
