75aa587a9ed9540fd976bb12988ac40ae55f3957
Niclas Cedermalm <niclas@cedermalm.se>
2016-10-27 17:10:52 +0200

    Added start/stop functionality.

--- framework/ecommerce-framework-odata-service/src/main/java/com/sdl/ecommerce/odata/service/ServiceContainer.java
+++ framework/ecommerce-framework-odata-service/src/main/java/com/sdl/ecommerce/odata/service/ServiceContainer.java
@@ -3,11 +3,13 @@ package com.sdl.ecommerce.odata.service;
 import com.sdl.odata.service.ODataServiceConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.*;
 
 /**
@@ -22,7 +24,6 @@ import org.springframework.context.annotation.*;
 @EnableAutoConfiguration(exclude = {HibernateJpaAutoConfiguration.class, DataSourceAutoConfiguration.class,
         DataSourceTransactionManagerAutoConfiguration.class})
 @Import({
-        //InMemoryDataSourceConfiguration.class,
         ODataServiceConfiguration.class
 })
 @ComponentScan (basePackages = {"com.sdl.ecommerce.odata"})
@@ -31,14 +32,28 @@ public class ServiceContainer {
 
     private static final Logger LOG = LoggerFactory.getLogger(ServiceContainer.class);
 
+    private static ApplicationContext applicationContext = null;
+
     public static void main(String[] args) {
-        LOG.info("Starting E-Commerce container");
 
-        SpringApplication springApplication = new SpringApplication(ServiceContainer.class);
-        springApplication.setShowBanner(false);
-        springApplication.run(args);
+        String mode = args != null && args.length > 0 ? args[0] : null;
+        if (applicationContext != null && mode != null && "stop".equals(mode)) {
+            System.exit(SpringApplication.exit(applicationContext, new ExitCodeGenerator() {
+                @Override
+                public int getExitCode() {
+                    return 0;
+                }
+            }));
+            LOG.info("E-Commerce Service application container stopped.");
+        }
+        else {
+            LOG.info("Starting E-Commerce container");
+            SpringApplication springApplication = new SpringApplication(ServiceContainer.class);
+            springApplication.setShowBanner(false);
+            applicationContext = springApplication.run(args);
+            LOG.info("E-Commerce Service application container started");
+        }
 
-        LOG.info("E-Commerce Service application container started");
     }
 
 }
