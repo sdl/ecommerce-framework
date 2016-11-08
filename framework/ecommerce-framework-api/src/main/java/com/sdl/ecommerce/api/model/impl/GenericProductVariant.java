cc0adcdcef88839b096609eb5c60fcfc2dece5c7
Niclas Cedermalm <ncedermalm@sdl.com>
2016-11-08 21:43:18 +0000

    Added a generic implementation of the ProductVariant interface.

---
+++ framework/ecommerce-framework-api/src/main/java/com/sdl/ecommerce/api/model/impl/GenericProductVariant.java
@@ -0,0 +1,40 @@
+package com.sdl.ecommerce.api.model.impl;
+
+import com.sdl.ecommerce.api.model.ProductPrice;
+import com.sdl.ecommerce.api.model.ProductVariant;
+import com.sdl.ecommerce.api.model.ProductVariantAttribute;
+
+import java.util.List;
+
+/**
+ * Generic Product Variant
+ *
+ * @author nic
+ */
+public class GenericProductVariant implements ProductVariant {
+
+    private String id;
+    private ProductPrice price;
+    private List<ProductVariantAttribute> attributes;
+
+    public GenericProductVariant(String id, ProductPrice price, List<ProductVariantAttribute> attributes) {
+        this.id = id;
+        this.price = price;
+        this.attributes = attributes;
+    }
+
+    @Override
+    public String getId() {
+        return this.id;
+    }
+
+    @Override
+    public ProductPrice getPrice() {
+        return this.price;
+    }
+
+    @Override
+    public List<ProductVariantAttribute> getAttributes() {
+        return this.attributes;
+    }
+}
