0aefccde4ff12dd2abb6b7fe0555dec6b58f62d8
Niclas Cedermalm <niclas@cedermalm.se>
2016-11-26 18:14:36 +0100

    Added example DXA.Java webapp for E-Commerce

---
+++ dxa.java/ecommerce-framework-example-webapp/src/main/java/com/sdl/ecommerce/webapp/SpringInitializer.java
@@ -0,0 +1,16 @@
+package com.sdl.ecommerce.webapp;
+
+import com.sdl.dxa.DxaSpringInitialization;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+/**
+ * Initializes Spring context for DXA web application.
+ */
+@Import(DxaSpringInitialization.class)
+@Configuration
+@ComponentScan("com.sdl.ecommerce.webapp")
+public class SpringInitializer {
+
+}
