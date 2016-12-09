ce3e08e6e1c5863ce30daa41bf0f7193d15cd38e
Örjan Eriksson <orjan.eriksson@squeed.com>
2016-12-09 15:05:38 +0100

    Added unit tests for HttpContextServant.

---
+++ dxa.net/ecommerce-framework-dotnet/Tests/SDL.ECommerce.UnitTests/Test.cs
@@ -0,0 +1,14 @@
+﻿namespace SDL.ECommerce.UnitTests
+{
+    using Ploeh.AutoFixture;
+
+    public abstract class Test
+    {
+        public IFixture Fixture { get; }
+
+        protected Test()
+        {
+            Fixture = new Fixture();
+        }
+    }
+}
