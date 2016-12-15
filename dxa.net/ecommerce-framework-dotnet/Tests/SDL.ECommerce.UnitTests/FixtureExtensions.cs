00a3bf77bb1ef3d72a7e686e98b144a859169ee3
Örjan Eriksson <orjan.eriksson@squeed.com>
2016-12-15 15:55:07 +0100

    Added fixture extension.

---
+++ dxa.net/ecommerce-framework-dotnet/Tests/SDL.ECommerce.UnitTests/FixtureExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace SDL.ECommerce.UnitTests
+{
+    using Ploeh.AutoFixture;
+
+    public static class FixtureExtensions
+    {
+        public static T GetStub<T>(this IFixture fixture)
+        {
+            return fixture.Create<T>();
+        }
+    }
+}
