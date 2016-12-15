c1e542d6d6821bb77e807193f4bca05f7f39c352
Örjan Eriksson <orjan.eriksson@squeed.com>
2016-12-15 11:57:40 +0100

    Added provider for test dependencies.

---
+++ dxa.net/ecommerce-framework-dotnet/Tests/SDL.ECommerce.UnitTests/DependencyTestProvider.cs
@@ -0,0 +1,34 @@
+﻿namespace SDL.ECommerce.UnitTests
+{
+    using System;
+    using System.Collections.Generic;
+
+    using Ploeh.AutoFixture;
+    using Ploeh.AutoFixture.Kernel;
+
+    using SDL.ECommerce.Api;
+    using SDL.ECommerce.DXA.Providers;
+    using SDL.ECommerce.DXA.Servants;
+
+    public class DependencyTestProvider
+    {
+        internal static void SetTestDependencies(ISpecimenBuilder fixture)
+        {
+            var dependencies = new Dictionary<Type, object>
+                                   {
+                                           { typeof(IECommerceClient), fixture.Create<IECommerceClient>() },
+                                           { typeof(IECommerceLinkResolver), fixture.Create<IECommerceLinkResolver>() },
+                                           { typeof(IHttpContextServant), fixture.Create<IHttpContextServant>() },
+                                           { typeof(IPageModelServant), fixture.Create<IPageModelServant>() },
+                                           { typeof(IPathServant), fixture.Create<IPathServant>() }
+                                   };
+
+            DependencyResolverProvider.Set(type => !dependencies.ContainsKey(type) ? null : dependencies[type]);
+        }
+
+        internal static void ResetDependencies()
+        {
+            DependencyResolverProvider.Reset();
+        }
+    }
+}
