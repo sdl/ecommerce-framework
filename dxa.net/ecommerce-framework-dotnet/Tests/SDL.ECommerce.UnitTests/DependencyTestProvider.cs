d6fc9600078fbd97cd49fb5be4ff43106f31cf87
Örjan Eriksson <orjan.eriksson@squeed.com>
2016-12-15 15:55:54 +0100

    Modified provider for test dependencies.

--- dxa.net/ecommerce-framework-dotnet/Tests/SDL.ECommerce.UnitTests/DependencyTestProvider.cs
+++ dxa.net/ecommerce-framework-dotnet/Tests/SDL.ECommerce.UnitTests/DependencyTestProvider.cs
@@ -4,29 +4,29 @@
     using System.Collections.Generic;
 
     using Ploeh.AutoFixture;
-    using Ploeh.AutoFixture.Kernel;
 
     using SDL.ECommerce.Api;
     using SDL.ECommerce.DXA.Providers;
     using SDL.ECommerce.DXA.Servants;
 
-    public class DependencyTestProvider
+    internal class DependencyTestProvider : IDisposable
     {
-        internal static void SetTestDependencies(ISpecimenBuilder fixture)
+        public DependencyTestProvider(IFixture fixture)
         {
             var dependencies = new Dictionary<Type, object>
                                    {
-                                           { typeof(IECommerceClient), fixture.Create<IECommerceClient>() },
-                                           { typeof(IECommerceLinkResolver), fixture.Create<IECommerceLinkResolver>() },
-                                           { typeof(IHttpContextServant), fixture.Create<IHttpContextServant>() },
-                                           { typeof(IPageModelServant), fixture.Create<IPageModelServant>() },
-                                           { typeof(IPathServant), fixture.Create<IPathServant>() }
+                                           { typeof(IECommerceClient), fixture.Freeze<IECommerceClient>() },
+                                           { typeof(IECommerceLinkResolver), fixture.Freeze<IECommerceLinkResolver>() },
+                                           { typeof(IHttpContextServant), fixture.Freeze<IHttpContextServant>() },
+                                           { typeof(IPageModelServant), fixture.Freeze<IPageModelServant>() },
+                                           { typeof(IPathServant), fixture.Freeze<IPathServant>() }
                                    };
 
             DependencyResolverProvider.Set(type => !dependencies.ContainsKey(type) ? null : dependencies[type]);
         }
 
-        internal static void ResetDependencies()
+
+        public void Dispose()
         {
             DependencyResolverProvider.Reset();
         }
