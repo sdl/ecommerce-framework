f760c3308adf4f6c68026ce40eade19c0911be46
Niclas Cedermalm <niclas@cedermalm.se>
2017-03-06 14:59:19 +0100

    Added new enum to the C# APIs for variant link types.

---
+++ dxa.net/ecommerce-framework-dotnet/SDL.ECommerce.Api/Model/VariantLinkType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDL.ECommerce.Api.Model
+{
+    /// <summary>
+    /// Variant Link Type
+    /// </summary>
+    public enum VariantLinkType
+    {
+        VARIANT_ID,
+        VARIANT_ATTRIBUTES
+    }
+}
