59ba0363865188c65e34a8c7607fd76e2dc079bf
Niclas Cedermalm <ncedermalm@sdl.com>
2017-12-13 15:10:43 +0100

    Updated assembly info to v1.2.0

--- dxa.net/ecommerce-framework-dotnet/SDL.ECommerce.DXA/Properties/AssemblyInfo.cs
+++ dxa.net/ecommerce-framework-dotnet/SDL.ECommerce.DXA/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SDL.ECommerce.DXA")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,7 +31,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
 
 [assembly: InternalsVisibleTo("SDL.ECommerce.UnitTests")]
