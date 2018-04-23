e4249b1b4bb8c0e636920638eda57d964f5b55c8
Niclas Cedermalm <ncedermalm@sdl.com>
2018-04-23 16:53:34 +0200

    Added a simple singleton for link resolving in the example views.

---
+++ dxa.net/ecommerce-framework-dotnet/SDL.ECommerce.DXA.Example.Views/LinkResolver.cs
@@ -0,0 +1,24 @@
+﻿using SDL.ECommerce.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SDL.ECommerce.DXA.Example.Views
+{
+    /// <summary>
+    /// Simple singleton to access the DXA link resolver
+    /// </summary>
+    public sealed class LinkResolver
+    {
+        private static readonly IECommerceLinkResolver linkResolver = new DXALinkResolver();
+
+        public static IECommerceLinkResolver Instance
+        {
+            get
+            {
+                return linkResolver;
+            }
+        }
+    }
+}
