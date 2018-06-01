72ed90e341a951e212732fa8e53564c25d8c0949
Niclas Cedermalm <ncedermalm@sdl.com>
2018-06-01 11:18:05 +0200

    Fredhopper ECL: Updated to v1.2.0.2

--- ecl/fredhopper-ecl-provider/fredhopper-ecl-provider/FredhopperEclProvider.cs
+++ ecl/fredhopper-ecl-provider/fredhopper-ecl-provider/FredhopperEclProvider.cs
@@ -5,7 +5,7 @@ using Tridion.ExternalContentLibrary.V2;
 
 namespace SDL.Fredhopper.Ecl
 {
-    [AddIn("Fredhopper-ECL-Provider", Version = "1.2.0.1")]
+    [AddIn("Fredhopper-ECL-Provider", Version = "1.2.0.2")]
     public class FredhopperEclProvider : EclProvider
     {
 
