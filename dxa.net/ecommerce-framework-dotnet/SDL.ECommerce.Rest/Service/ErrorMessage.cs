c957cd869a56a3bac1a764a4b985dcee8e4be064
Niclas Cedermalm <ncedermalm@sdl.com>
2019-01-28 10:20:15 +0000

    Added missing source file

---
+++ dxa.net/ecommerce-framework-dotnet/SDL.ECommerce.Rest/Service/ErrorMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDL.ECommerce.Rest.Service
+{
+    public class ErrorMessage
+    {
+        public string Message { get; set; }
+        public string Detail { get; set; }
+    }
+}
